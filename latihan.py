a835c6d7b8fa2267e8321df9e198cd8dc81a51d9
Khanza <khanzaprp@gmail.com>
2023-05-25 15:24:00 +0700

    func update error

--- latihan.py
+++ latihan.py
@@ -1,190 +1,174 @@
-# Type your answere here
-
 import sys
-import pyinputplus as pypi
+import pyinputplus as pyip
 
-def show(Dict, printFormat, title="\nDaftar Buah yang Tersedia\n"):
-    """_summary_
-
-    Args:
-        Dict (dictionary): dict yang akan ditampilkan
-        printFormat (string): format tampilan di prompt
-        title (str, optional): judul tampilan. Defaults to "\nDaftar Buah yang Tersedia\n".
-    """
-    # Menampilkan judul
-    print(title)
-    # Loop item di dalam listFruit
-    for value in Dict.values():
-        # Menampilkan item berdasarkan format
-        print(printFormat.format("", *value))
-
-
-def add():
-    # Meminta input buah, jumlah, dan harga
-    nameFruit = pypi.inputStr(prompt='Masukan nama buah : ', applyFunc=lambda x: x.capitalize(), blockRegexes=[r'[0-9]'])
-    countFruit = pypi.inputInt(prompt='Masukkan jumlah buah: ', greaterThan=0,blockRegexes=[r'[a-zA-Z]'])
-    priceFruit = pypi.inputInt(prompt='Masukkan harga buah: ', greaterThan=0,blockRegexes=[r'[a-zA-Z]'])
-    # Loop item di dalam listFruit
-    for key, value in listFruit.items():
-        # Apabila buah sudah ada di dalam daftar
-        if nameFruit in value:
-            listFruit[key][2] += countFruit
-            listFruit[key][3] = priceFruit
-            break
-    # Apabila buah tidak ada di dalam daftar
+def Main_Menu():
+    print(f'''
+Selamat Datang di Gudang Minimarket JCDS
+    ''')
+    # menginput fitur yang ingin ditampilkan 
+    prompt = "Masukan angka menu yang ingin dijalankan\n"
+    choice = ['Menampilkan list menu', 'Update Stock terkini','Add Stock Card', 'Delete Data Stock Card', 'Exit']
+    response = pyip.inputMenu(prompt=prompt, choices=choice, numbered=True)
+    print(response)
+    # Fitur menampilkan daftar sub menu
+    if response == choice[0]:
+        Sub_Menu()
+    # Fitur merubah data
+    elif response == choice[1]:
+        Update_Data_SC()
+    #     # Fitur menambah data
+    elif response == choice[2]:
+        Add_SC()
+    #     # Fitur menghapus data
+    elif response == choice[3]:
+        Delete_SC()
+    #     # Fitur exit program
     else:
-        index = len(listFruit) - 1
-        listFruit.update({
-            f'buah-{index}': [
-                index, 
-                nameFruit,
-                countFruit,
-                priceFruit
-            ]
-            }
-        )
-    # Menampilkan daftar buah terbaru
-    show(listFruit, printFormat)
+         sys.exit()
 
+def Tampilan_SubMenu_Utama(menu):
+    # Function to display the menu
+    for item in menu:
+        print(item)
 
-def delete():
-    # Input indeks buah yang akan dihapus
-    index = pypi.inputInt(prompt='Masukkan indeks buah yang ingin dihapus: ', greaterThan=0,blockRegexes=[r'[a-zA-Z]'])
+def show_List_Barang(Dict, FuncFormat, title="\nDaftar Barang yang Tersedia\n"):
+    # Function to show the list of available items
+    print(title)
+    for value in Dict.values():
+        print(FuncFormat.format("", *value))
+
+def show_Tabel_SC(Dict, Format_SC, titleSC):
+    # Function to display the stock card table
+    print(titleSC)
+    for value in Dict.values():
+        print(Format_SC.format("", *value))
+
+def Sub_Menu():
+    ListBarang = {
+        'column': ["Index", "Kode Barang", "Nama Barang", "Jenis Barang", "Stock Terkini", "Level Stock"],
+        'barang1': [0, 101, "Mie Instan", "Makanan", 100, "low stock"],
+        'barang2': [1, 102, "Sereal", "Makanan", 150, "available"],
+        'barang3': [2, 201, "Susu", "Minuman", 150, "available"],
+        'barang4': [3, 202, "Kopi", "Minuman", 150, "available"],
+    }
+
+    FuncFormat = "{:<4}" + "{:<8}" + "{:<15}" + "{:<15}" + "{:<15}"+ "{:<15}"+ "{:<15}"
+
+    while True:
+        prompt = "Masukkan angka menu yang ingin dijalankan:\n"
+        ListMenu = [
+            "1. Daftar Stock Barang Terkini",
+            "2. Stock Card Product",
+            "3. Back"
+        ]
+        Tampilan_SubMenu_Utama(ListMenu)
+        response = input(prompt)
+
+        if response == '1':
+            show_List_Barang(ListBarang, FuncFormat)
+        elif response == '2':
+            show_Tabel_SC(Stock_Card, Format_SC, titleSC)
+        elif response == '3':
+            sys.exit()
+
+def Add_SC(): #kurang menampilkan balik ke main menu dan print transaksi sudah ada tdk dapat ditampilkan
+    # update data stock card
+    tanggal = pyip.inputStr(prompt='Masukan Tanggal Transaksi (YYYY/MM/DD) : ', blockRegexes=[r'[a-zA-Z]'])
+    jenis_transaksi = pyip.inputStr(prompt='Masukkan Jenis Transaksi : ', applyFunc=lambda x: x.title(), blockRegexes=[r'[0-9]'])
+    qty = pyip.inputInt(prompt='Masukan Qty Barang :', blockRegexes=[r'[a-zA-Z]'], greaterThan=0)
+
+    # Loop item di dalam Stock_Card
+    for key, value in Stock_Card.items():
+        # Apabila transaksi sudah ada di dalam daftar
+        if key == 'column':
+            continue
+        elif tanggal and jenis_transaksi in value:
+            print('Transaksi sudah ada')
+            break
+    # Apabila transaksi tidak ada di dalam daftar
+        else:    
+            kode_barang = pyip.inputInt(prompt='Masukkan Kode Barang : ', greaterThan=0,blockRegexes=[r'[a-zA-Z]']) 
+            harga = pyip.inputInt(prompt='Masukan Harga Barang :', blockRegexes= float and [r'[a-zA-Z]'], greaterThan=000)
+            stock_balance = pyip.inputInt(prompt='Masukan Stock Barang :', blockRegexes= float and [r'[a-zA-Z]'], greaterThan=0)
+            add = pyip.inputYesNo(prompt="\nIngin menambahkan data?(yes/no): ")
+            if add == 'yes':
+                index = len(Stock_Card) - 1
+                Stock_Card.update({
+                    f'transaksi{index}': [
+                        index, 
+                        tanggal,
+                        kode_barang,
+                        jenis_transaksi,
+                        qty,
+                        harga,
+                        stock_balance
+                    ]
+                    }
+                )
+                break
+            else:
+                break      
+    # Menampilkan daftar stock data terbaru
+    show_Tabel_SC(Stock_Card, Format_SC,titleSC)
+
+def Delete_SC():
+    # Input indeks SC yang akan dihapus
+    index = pyip.inputInt(prompt='Masukkan indeks Stock Card yang ingin dihapus: ', greaterThan=0,blockRegexes=[r'[a-zA-Z]'])
     # Menghapus buah berdasarkan indeks
-    del listFruit[f'buah-{index}']
+    del Stock_Card[f'transaksi{index}']
     # Update indeks buah yang tersisa
-    for key, value in listFruit.items():
+    for key, value in Stock_Card.copy().items():
         if key != 'column' and value[0] > index:
-            del listFruit[key]
-            listFruit.update({
-                f'buah-{value[0] - 1}': [
+            del Stock_Card[key]
+            Stock_Card.update({
+                f'transaksi{value[0] - 1}': [
                     value[0]-1, 
                     value[1], 
                     value[2], 
-                    value[3]]
-                }
-            )
-    # Menampilkan daftar buah terbaru
-    show(listFruit, printFormat)
-
-
-def buy():
-    # Deklarasi variabel 'listChart'
-    listChart = {
-        'column': ["nama", "qty", "harga"],
-    }
-    while True:
-        # Menampilkan data buah terbaru
-        show(listFruit, printFormat)
-        # Input indeks dan jumlah buah yang akan dibeli
-        index = pypi.inputInt(prompt="\nMasukkan indeks buah yang ingin dibeli: ", min=0,lessThan=len(listFruit) -1, blockRegexes=[r'[a-zA-Z]'])
-        index, nameFruit, stock, price = listFruit[f'buah-{index}']
-        countFruit = pypi.inputInt(prompt='Masukkan jumlah yang ingin dibeli: ', greaterThan=0, max=stock, blockRegexes=[r'[a-zA-Z]'])
-        # Jika jumlah pesanan tidak terpenuhi, tampilkan pesan stock kurang
-        if countFruit > stock:
-            print(f"Stock tidak cukup, stock {nameFruit} tinggal {stock}")
-        # Selain itu, tambahkan ke 'listChart'
-        else:
-            index = len(listChart) - 1
-            listChart.update({
-                f'buah-{index}': [
-                    nameFruit,
-                    countFruit,
-                    price
+                    value[3],
+                    value[4],
+                    value[5],
+                    value[6]
+                    # value[7]
                     ]
                 }
             )
-            # Kurangi persedian stock buah yang dipesan
-            nameFruit[2] -= countFruit
-            # Tampilkan isi keranjang belanjaan
-            chartFormat = "{:<4}" + "{:<10}" * (len(listChart['column']))
-            show(listChart, chartFormat, title="\nIsi Keranjang Anda\n")
-            # Konfirmasi user apakah akan re-order
-            reorder = pypi.inputYesNo(prompt="\nIngin beli yang lain?(yes/no): ")
-            # if reorder == 'no':
-            break
+    # Menampilkan daftar buah terbaru
+    show_Tabel_SC(Stock_Card, Format_SC,titleSC)
 
-    # Proses kalkulasi total harga
-    for key, value in listChart.items():
+def Update_Data_SC():
+    tanggal = pyip.inputStr(prompt='Masukan Tanggal Transaksi (YYYY/MM/DD) : ', blockRegexes=[r'[a-zA-Z]'])
+    jenis_transaksi = pyip.inputStr(prompt='Masukkan Jenis Transaksi : ', applyFunc=lambda x: x.capitalize(), blockRegexes=[r'[0-9]'])
+    qty = pyip.inputInt(prompt='Masukan Qty Barang :', blockRegexes=[r'[a-zA-Z]'], greaterThan=0),
+    kode_barang = pyip.inputInt(prompt='Masukkan Kode Barang : ', greaterThan=0,blockRegexes=[r'[a-zA-Z]']) 
+    harga = pyip.inputInt(prompt='Masukan Harga Barang :', blockRegexes= float and [r'[a-zA-Z]'], greaterThan=000)
+    stock_balance = pyip.inputInt(prompt='Masukan Stock Barang :', blockRegexes= float and [r'[a-zA-Z]'], greaterThan=0)
+    for key, value in Stock_Card.copy.items():
         if key == 'column':
-            value.append('total harga')
-            listChart[key] = value
-        else:
-            # Kalkulasi Qty x Harga
-            value.append(value[1] * value[2])
-            listChart[key] = value
-
-    # Proses pembayaran
-    while True:
-        # Menampilkan daftar belanja
-        show(listChart, printFormat, title="\nDaftar Belanjaan Anda\n")
-        # Hitung total harga yang harus dibayar
-        price = 0
-        for value in listChart.values():
-            if value[-1] != 'total harga':
-                price += value[-1]
-        print(f"\nTotal yang harus dibayar: {price}")
-        # Input jumlah uang pembayaran
-        pay = pypi.inputInt(prompt='Masukkan jumlah uang: ',blockRegexes=[r'[a-zA-Z]'])
-        # Jika uang kurang, tampilkan pesan uang kurang
-        # Minta user input ulang jumlah uang pembayaran
-        if pay - price < 0:
-            print(f"Uang anda kurang sebesar {abs(pay - price)}")
-        # Sebaliknya, tampilkan kembalian dan terima kasih
-        else:
-            print(f"Uang kembalian anda {pay - price}, terima kasih.")
+            continue
+        elif jenis_transaksi in value:
+            Stock_Card[key][1] = tanggal
+            Stock_Card[key][2] = kode_barang
+            Stock_Card[key][3] = jenis_transaksi
+            Stock_Card[key][4] = qty
+            Stock_Card[key][5] = harga
+            Stock_Card[key][6] = stock_balance
             break
-    # Clear keranjang belanja
-    del listChart
-
-
-def main():
-    while True:
-        # Menampilkan tampilan utama program
-#         print(
-#             """
-# Selamat datang di pasar buah
-
-#     List menu:
-
-#     1. Menampilkan daftar buah
-#     2. Menambah buah
-#     3. Menghapus buah
-#     4. Membeli buah
-#     5. Exit
-# """
-#         )
-        # Input fitur yang akan dijalankan
-        prompt = "Masukan angka menu yang ingin dijalankan\n"
-        choice = ['Menampilkan daftar buah', 'Menambah buah', 'Menghapus buah', 'Membeli buah', 'Exit']
-        response = pypi.inputMenu(prompt=prompt, choices=choice, numbered=True)
-        print(response)
-        # Fitur menampilkan daftar buah
-        if response == choice[0]:
-            show(listFruit, printFormat)
-        # Fitur menambahkan buah
-        elif response == choice[1]:
-            add()
-        # Fitur menghapus buah
-        elif response == choice[2]:
-            delete()
-        # Fitur membeli buah
-        elif response == choice[3]:
-            buy()
-        # Fitur exit program
         else:
-            sys.exit()
+            continue
+    show_Tabel_SC(Stock_Card,Format_SC,titleSC)
 
 
-if __name__ == "__main__":
-    # Deklrasi variabel 'listFruit'
-    listFruit = {
-        'column': ["index", "nama", "stock", "harga"],
-        'buah-0': [0, "Apel", 20, 10000],
-        'buah-1': [1, "Anggur", 25, 20000],
-        'buah-2': [2, "Jeruk", 15, 15000],
+if __name__ == '__main__':
+    Stock_Card = {
+        'column': ["Index", "Tanggal", "Kode Barang", "Jenis Transaksi", "Qty", "Harga", "Stock Balance"],
+        'transaksi1': [0, '2023/01/01', 101, "Saldo Awal", 150, 2000, 150],
+        'transaksi2': [1, '2023/01/01', 102, "Saldo Awal", 150, 20000, 150],
+        'transaksi3': [2, '2023/01/01', 201, "Saldo Awal", 150, 5000, 150],
+        'transaksi4': [3, '2023/01/01', 202, "Saldo Awal", 150, 8000, 150],
+        'transaksi5': [4, '2023/01/05', 101, "Penjualan", 50, 2000, 100]        
     }
-    # Deklarasi format tampilan di prompt
-    printFormat = "{:<4}" + "{:<10}" * (len(listFruit['column']))
-    # Menjalankan fungsi utama main()
-    main()
+    
+    titleSC = '\n Stock Card \n'
+    Format_SC = "{:<4}" + "{:<8}" + "{:<15}" + "{:<15}" + "{:<20}"+ "{:<8}"+ "{:<10}" + "{:<15}"
+    Main_Menu()
